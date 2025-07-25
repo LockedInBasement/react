56e5da70bcb2405404a642d57458074bf3c2c015
LockedInBasement <dariusz.strych.lib@gmail.com>
2025-07-25 12:01:21 +0200

    fisrt commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import './App.css'
+import Footer from './Footer';
+
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ myapp/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+return(
+    <footer>
+        <p>&copy; {new Date().getFullYear()} Your website name</p>
+        <p></p>
+    </footer> 
+    );
+}
+
+export default Footer
