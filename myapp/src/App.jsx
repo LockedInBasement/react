872095c5de4fd75d71e1a7bbd192022a8664fc30
LockedInBasement <dariusz.strych.lib@gmail.com>
2025-07-25 12:12:16 +0200

    First lesson done

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './Header'
 import './App.css'
 import Footer from './Footer';
+import Food from './Food';
 
 function App() {
 
   return (
     <>
     <Header></Header>
+    <Food></Food>
     <Footer></Footer>
     </>
   );
